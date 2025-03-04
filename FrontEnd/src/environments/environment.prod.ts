1204c40c147ae6bab13fe202ab8b40bcaa7860e0
thiyari <manikanth578@gmail.com>
2025-03-04 22:53:10 +0530

    update env

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     PAYPAL_CLIENT_ID: "AYaJue-5Dr7hIopc-lw7y-3utpPUOJ5O_u9PW2B0Oi0QTbRiB6ov3Acu-xdJLP9FB6kiMyUut3kDh3Zo",
     RAZOR_PAY_KEY: "rzp_test_pwqgLBWfRp7Dgx",
     SERVER_LOCAL_URI: "http://localhost:8086",
-    SERVER_URI: "https://cart-server-vfnm.onrender.com",
+    SERVER_URI: "https://shoppingcart-k9dy.onrender.com",
     CLIENT_LOCAL_URI: "http://localhost:4200",
     CLIENT_URI: "https://cartclient-c110f.web.app",
     GOOGLE_PAY_GATEWAY: "example",
