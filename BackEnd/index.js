6339fa4fa53b6585ec2939c70a8f897f94ca8510
thiyari <manikanth578@gmail.com>
2025-03-13 21:41:18 +0530

    update

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -23,7 +23,6 @@ app.use(cors(
 app.use(express.json({limit: "50mb"})); // setting limit to 50 MB (52428800 in bytes)
 app.use(express.urlencoded({limit: "50mb", extended: true , parameterLimit: 100000}));
   
-app.set('trust proxy', 1)
 app.use(cookieParser());
 app.use(bodyParser.json())
 app.use(session({
