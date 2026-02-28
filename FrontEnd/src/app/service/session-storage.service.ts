a0531c5188c31ec119ccd3958bc69f96aeca7ba1
thiyari <manikanth578@gmail.com>
2026-02-28 21:17:01 +0530

    update

--- FrontEnd/src/app/service/session-storage.service.ts
+++ FrontEnd/src/app/service/session-storage.service.ts
@@ -1,43 +1,55 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionStorageService {
 
-  constructor() { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
 
   setItem(key: string, value: any, expiryInMinutes: number): void {
-    const now = new Date();
-    const item = {
-      value: value,
-      expiry: now.getTime() + expiryInMinutes * 60 * 1000, // Convert minutes to milliseconds
-    };
-    sessionStorage.setItem(key, JSON.stringify(item));
+      if (isPlatformBrowser(this.platformId)) {
+
+        const now = new Date();
+        const item = {
+          value: value,
+          expiry: now.getTime() + expiryInMinutes * 60 * 1000, // Convert minutes to milliseconds
+        };
+        sessionStorage.setItem(key, JSON.stringify(item));
+      }
   }
 
-  getItem(key: string): any {
-    const itemStr = sessionStorage.getItem(key);
-    if (!itemStr) {
-      return null;
-    }
+  getItem(key: string): any | null{ 
+    if (isPlatformBrowser(this.platformId)) {
+        const itemStr = sessionStorage.getItem(key);
+        if (!itemStr) {
+          return null;
+        }
 
-    const item = JSON.parse(itemStr);
-    const now = new Date();
+        const item = JSON.parse(itemStr);
+        const now = new Date();
 
-    // Check if the item is expired
-    if (now.getTime() > item.expiry) {
-      sessionStorage.removeItem(key);
-      return null;
+        // Check if the item is expired
+        if (now.getTime() > item.expiry) {
+          sessionStorage.removeItem(key);
+          return null;
+        }
+        return item.value;
     }
-    return item.value;
+    return null;
   }
 
   removeItem(key: string): void {
-    sessionStorage.removeItem(key);
+    if (isPlatformBrowser(this.platformId)) {
+      sessionStorage.removeItem(key);
+    }
   }
 
   clear(): void {
-    sessionStorage.clear();
+    if (isPlatformBrowser(this.platformId)) {
+      sessionStorage.clear();
+    }
   }
 }
